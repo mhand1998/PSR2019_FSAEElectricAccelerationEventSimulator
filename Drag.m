732ab566ab5a3229414577055ce9584377f575f5
mhand1998 <michaelhand1998@gmail.com>
2018-04-16 18:38:41 -0400

    edited drag function

--- Drag.m
+++ Drag.m
@@ -1,6 +1,6 @@
 function [DRAG_FORCE] = Drag(cd,frontal_area,air_density,velocity)
 %Drag: Uses the drag equation to calculate drag on object
-DRAG_FORCE = frontal_area.*cd.*(air_density.*velocity^2/2);
+DRAG_FORCE = frontal_area.*cd.*(air_density*velocity^2/2);
 
 end
 
