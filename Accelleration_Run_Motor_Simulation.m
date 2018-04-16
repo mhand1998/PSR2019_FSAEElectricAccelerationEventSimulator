edc769d9450a02c527fc52372140037faa2f7d27
mhand1998 <michaelhand1998@gmail.com>
2018-04-16 19:07:59 -0400

    Fixed Drag Array

--- Accelleration_Run_Motor_Simulation.m
+++ Accelleration_Run_Motor_Simulation.m
@@ -23,39 +23,13 @@ load(fullfile(PathName,vehicleProfile))
 motor = csvread(fullfile(PathName,TSCurveFile));
 
 %%
-
-%Enter Motor Electrical Data
-
-currentAnswer = questdlg('Is Current vs. Torque Linear?');
-switch currentAnswer
-    case 'Yes'
-        torque_constant = inputdlg('Enter Torque Constant[Nm/A rms]');
-        delimiter = 1;
-        torque_constant = str2double(torque_constant);
-    case 'No'
-        %CTCurveFile = uigetfile('','Slect Motor Current vs Torque','*.csv');
-        %motorCurrentCurve = csvread(TSCurveFile);
-        %delimiter = 2;
-        torque_constant = inputdlg('Enter Torque Constant[Nm/A rms]');
-        delimiter = 1;
-        torque_constant = str2double(torque_constant);
-end
-
-BackEMF_constant = inputdlg('Enter Back EMF Constant [RPM/V rms]');
-BackEMF_constant = str2double(BackEMF_constant);
-winding_resistance = inputdlg('Enter Motor Winding Resistance [Ohms]');
-winding_resistance = str2double(winding_resistance);
-peak_full_load_speed = inputdlg('Enter Peak Speed [RPM]');
-peak_full_load_speed = str2double(peak_full_load_speed);
-
-%%
 %Load Motor Profile
 
 [motorProfile,PathName] = uigetfile('','Load Motor Profile','*.mat');
 load(fullfile(PathName,motorProfile))
 
 %%
-cd C:\Users\Michael\Documents\GitHub\FSAE_Electric_Acceleration_Event_Simulator
+%cd C:\Users\Michael\Documents\GitHub\FSAE_Electric_Acceleration_Event_Simulator
 
 gear_ratio = 3.80; %[] parameter of powertrain
 
